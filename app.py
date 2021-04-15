dee6656b9586e473695b091872618177aca50663
Herson Oliveira <oliveira.herson@gmail.com>
2021-04-15 21:12:30 +0200

    Add template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -7,3 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello World"
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/add_game", methods=["GET", "POST"])
+def add_game():
+    if request.method == "POST":
+        for key, value in request.form.items():
+            print(f"key: {key} | value: {value}")
+    return render_template("add_game.html")
