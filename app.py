e79d0e7afcc674bf8374ca7b3785966c722ce936
Herson Oliveira <v-holiveira@expediagroup.com>
2021-04-23 16:50:38 +0200

    Move games templates

--- app.py
+++ app.py
@@ -1,25 +1,4 @@
-from flask import render_template, request, session, url_for, redirect
 from pokerena import create_app
 
 
 app = create_app()
-
-
-@app.route("/add_game", methods=["GET", "POST"])
-def add_game():
-    if request.method == "POST":
-        for key, value in request.form.items():
-            print(f"key: {key} | value: {value}")
-
-        # Save form data to the session object
-        session["game_name"] = request.form["game_name"]
-        session["date"] = request.form["date"]
-        session["players"] = request.form["players"]
-        return redirect(url_for("list_games"))
-
-    return render_template("add_game.html")
-
-
-@app.route("/list_games")
-def list_games():
-    return render_template("games.html")
--- pokerena/games/routes.py
+++ pokerena/games/routes.py
@@ -1,7 +1,27 @@
 from . import games_blueprint
-from flask import render_template
+from flask import render_template, request, session, redirect, url_for
 
 
 @games_blueprint.route("/")
 def index():
     return render_template("games/index.html")
+
+
+@games_blueprint.route("/add_game", methods=["GET", "POST"])
+def add_game():
+    if request.method == "POST":
+        for key, value in request.form.items():
+            print(f"key: {key} | value: {value}")
+
+        # Save form data to the session object
+        session["game_name"] = request.form["game_name"]
+        session["date"] = request.form["date"]
+        session["players"] = request.form["players"]
+        return redirect(url_for("games.list_games"))
+
+    return render_template("games/add_game.html")
+
+
+@games_blueprint.route("/list_games")
+def list_games():
+    return render_template("games/games.html")
