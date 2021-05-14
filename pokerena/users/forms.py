d99af80ce66d4abb8db740ce5d95259a584f17d9
hersonoliveira <oliveira.herson@gmail.com>
2021-05-14 11:48:21 +0200

    Update form

--- pokerena/users/forms.py
+++ pokerena/users/forms.py
@@ -4,6 +4,11 @@ from wtforms.validators import DataRequired, Length, Email
 
 
 class RegistrationForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
     email = StringField(
         "Email", validators=[DataRequired(), Email(), Length(min=6, max=120)]
     )
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    submit = SubmitField("Register")
