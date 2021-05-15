98052eade0f539a00653740426a65ab8a69dd869
Guilherme Heimbecker Eisfeld <guilherme.eisfeld@compasso.com.br>
2021-05-14 21:51:10 -0300

    fixing develop

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
-import Link from "next/link";
+  import Link from "next/link";
 import { css } from "@emotion/css";
 import Amplify from "aws-amplify";
-import config from "../samrc/aws-exports";
+import config from "../src/aws-exports";
 Amplify.configure({
   ...config,
   ssr: true,
