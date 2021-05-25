3941390ed199ce6a42ce4474b149112da8ddfe53
hersonoliveira <oliveira.herson@gmail.com>
2021-05-25 18:52:36 +0200

    Delete files from root

--- app.py
+++
@@ -1,4 +0,0 @@
-from pokerena import create_app
-
-
-app = create_app()
--- pokerena/games/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-games_blueprint = Blueprint("games", __name__, template_folder="templates")
-
-from . import routes
